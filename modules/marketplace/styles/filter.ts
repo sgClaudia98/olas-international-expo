3b74c23f9d02b6e40e98ab7eec21e95f360e2ebc
hserra1806 <hserra.morejon@gmail.com>
2025-03-13 02:52:10 +0100

    feature: Filter Drawer for mobile view

--- modules/marketplace/styles/filter.ts
+++ modules/marketplace/styles/filter.ts
@@ -1,26 +1,46 @@
-import {Colors} from '@/styles';
-import {layout} from '@/styles/sizing';
-import {StyleSheet} from 'react-native';
+import { Colors } from "@/styles";
+import { layout } from "@/styles/sizing";
+import { StyleSheet } from "react-native";
 
 const styles = {
   mobile: StyleSheet.create({
     filters: {
-      display: 'none',
+      padding: 20,
+      gap: 5,
     },
     filterHeader: {
-      display: 'none',
+      fontSize: 14,
+      fontWeight: "bold",
+      marginBottom: 10,
     },
   }),
   tablet: StyleSheet.create({
     filters: {
-      backgroundColor: '#f4f4f4',
-      padding: 10,
-      width: 250, // Fixed width for left panel
+      backgroundColor: Colors.black.fifth,
+      borderRadius: 8,
+      padding: 20,
+      width: 225, // Fixed width for left panel
       gap: 5,
+      alignSelf: 'stretch',
     },
     filterHeader: {
       fontSize: 18,
-      fontWeight: 'bold',
+      fontWeight: "bold",
+      marginBottom: 10,
+    },
+  }),
+  desktop: StyleSheet.create({
+    filters: {
+      backgroundColor: Colors.black.fifth,
+      borderRadius: 8,
+      padding: 20,
+      width: 225, // Fixed width for left panel
+      gap: 5,
+      alignSelf: 'stretch',
+    },
+    filterHeader: {
+      fontSize: 18,
+      fontWeight: "bold",
       marginBottom: 10,
     },
   }),
