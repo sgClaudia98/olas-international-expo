ce3a600ae00b5b63a89e2ea441683ea962df3cca
hserra1806 <hserra.morejon@gmail.com>
2025-03-21 11:09:01 +0100

    refactor: page.tsx

--- components/layout/Page.tsx
+++ components/layout/Page.tsx
@@ -8,22 +8,20 @@ interface PageProps {
   children: ReactNode; // Define el tipo para los hijos del componente
 }
 
-
 // This needs to be refactored so you can place a footer dynamically from outside this component using the LayoutContext
 const Page: React.FC<PageProps> = ({ children }) => {
   const styles = useResponsiveStyles(responsiveStyle);
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.scrollContent} showsHorizontalScrollIndicator={false}>
-      <View
-          style={styles.pageContainer}>
-          {children}
-        </View>
+      <ScrollView
+        style={styles.scrollContent}
+        showsHorizontalScrollIndicator={false}
+      >
+        <View style={styles.pageContainer}>{children}</View>
         <Footer />
       </ScrollView>
     </SafeAreaView>
   );
 };
-
 
 export default Page;
