1301321846094d913791e4dda942f9cffeb16f0c
Claudia SG <claudia.sg2611@gmail.com>
2025-03-26 13:59:47 +0100

    add: Data link on order detail

--- app/(auth)/profile/order-history/[id].tsx
+++ app/(auth)/profile/order-history/[id].tsx
@@ -4,10 +4,15 @@ import { ThemedView } from "@/components/ThemedView";
 import { ThemedText } from "@/components/ThemedText";
 import { OrderHistory } from "@/modules/marketplace/pages/OrderHistory";
 import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+import { OrderDetail } from "@/modules/marketplace/pages/OrderDetail";
 
 export default function OrdersScreen() {
-  return <View>
-        <ThemedText>HOla</ThemedText>
-    </View>
-    ;
+  const { id } = useLocalSearchParams();
+  
+  if (typeof id !== "string") {
+    return <ThemedText>Invalid order ID</ThemedText>; // Handle the case where id is not a string
+  }
+  
+  return <OrderDetail id={id}/>
 }
--- modules/marketplace/pages/OrderDetail.tsx
+++ modules/marketplace/pages/OrderDetail.tsx
@@ -9,20 +9,17 @@ import { useTranslation } from "react-i18next";
 import { useSearchMarketBookingsMutation } from "../services/api/BookingService";
 import { ActivityIndicator } from "react-native-paper";
 
-const MAX_PER_PAGE = 5;
-
-export const OrderHistory: FC<ViewProps> = () => {
+export const OrderDetail: FC<{ id: string }> = ({ id }) => {
   const router = useRouter();
   const { t } = useTranslation();
   const styles = useResponsiveStyles(orderStyles);
 
-  const [searchMarketOptions, { data , isLoading}] =
+  const [searchMarketOptions, { data, isLoading }] =
     useSearchMarketBookingsMutation();
 
   useEffect(() => {
     searchMarketOptions({
-      offset: 0,
-      limit: MAX_PER_PAGE,
+      id: +id,
     });
   }, []);
 
@@ -31,19 +28,18 @@ export const OrderHistory: FC<ViewProps> = () => {
       <View style={styles.card}>
         <View style={styles.cardHeader}>
           <ThemedText style={styles.cardHeaderText}>
-            {t("PAGE.ORDER_HISTORY")}
+            {t("PAGE.ORDER_DETAIL")}
           </ThemedText>
+          {/**AQUI FALTA EL BACK Link */}
         </View>
         <View style={styles.cardContent}>
-          {isLoading && <ActivityIndicator/>}
+          {isLoading && <ActivityIndicator />}
           {data?.value.bookings.map((booking) => (
-            <View style={{flexDirection: "row", gap: 10}}>
-
-            <ThemedText>{booking.reference}</ThemedText>
-            <ThemedText>{booking.bookingDate}</ThemedText>
-            <ThemedText>{booking.totalPrice} USD</ThemedText>
-            <ThemedText>{booking.status}</ThemedText>
-            <Link href={`/profile/order-history/${booking.id}`}>eye icon</Link>
+            <View>
+              <ThemedText>{booking.reference}</ThemedText>
+              <ThemedText>{booking.bookingDate}</ThemedText>
+              <ThemedText>{booking.totalPrice} USD</ThemedText>
+              <ThemedText>{booking.status}</ThemedText>
             </View>
           ))}
         </View>
