7fec99486b44509358d1478e9b8b95edc3ed39cf
hserra1806 <hserra.morejon@gmail.com>
2025-03-27 14:32:34 +0100

    add: passing styles through a prop to ContentBox component

--- modules/marketplace/components/payment/ContentBox.tsx
+++ modules/marketplace/components/payment/ContentBox.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ViewStyle } from "react-native";
 import { Colors } from "@/styles";
 import typography from "@/styles/typography";
 
 interface ContentBoxProps {
   title?: string;
   data: Record<string, { icon?: JSX.Element; value: string } | string>;
+  contentBoxStyle?: ViewStyle;
   backgroundColor: string;
   shadow?: boolean;
 }
@@ -14,10 +15,17 @@ const ContentBox: React.FC<ContentBoxProps> = ({
   title,
   data,
   backgroundColor,
+  contentBoxStyle = {
+    borderRadius: 8,
+    minHeight: 240,
+    marginHorizontal: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 18,
+  },
   shadow,
 }) => {
   return (
-    <View style={[styles.contentBox, shadow ? styles.shadowStyles : '', { backgroundColor }]}>
+    <View style={[contentBoxStyle, shadow ? styles.shadowStyles : '', { backgroundColor }]}>
       {title && <Text style={styles.title}>{title}</Text>}
       {Object.entries(data).map(([key, item]) => {
         const { icon, value } =
@@ -44,13 +52,6 @@ const styles = StyleSheet.create({
     color: Colors.black.primary,
     fontWeight: "600",
     marginBottom: 15,
-  },
-  contentBox: {
-    borderRadius: 8,
-    paddingHorizontal: 20,
-    paddingVertical: 18,
-    minHeight: 240,
-    marginHorizontal: 5,
   },
   shadowStyles: {
     elevation: 3, // shadow in Android
